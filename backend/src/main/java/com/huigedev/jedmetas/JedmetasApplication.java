e01bf76e58c2c634dcce7e3bb1147cb58f5635d4
Carlos-Huigens <huige.dev@gmail.com>
2022-09-11 22:28:20 -0300

    Criação do projeto

---
+++ backend/src/main/java/com/huigedev/jedmetas/JedmetasApplication.java
@@ -0,0 +1,13 @@
+package com.huigedev.jedmetas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JedmetasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JedmetasApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/huigedev/jedmetas/JedmetasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huigedev.jedmetas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JedmetasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
